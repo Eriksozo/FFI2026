d360746e0b680bbfdadee564d6f0de4371e7146a
Eriksozo <erik.iplou@icloud.com>
2026-05-30 15:43:57 -0400

    Delete ffi-nextjs directory

--- ffi-nextjs/components/Rich.jsx
+++
@@ -1,4 +0,0 @@
-// Tiny helper to render the rich (bold/underline/color) copy strings.
-export default function Rich({ html, as: Tag = 'span', className }) {
-  return <Tag className={className} dangerouslySetInnerHTML={{ __html: html }} />;
-}
